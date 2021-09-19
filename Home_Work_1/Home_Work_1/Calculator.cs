d9737dab69b6f02532a800cab80d8b156309f60e
touou <72223183+touou@users.noreply.github.com>
2021-09-20 00:20:34 +0300

    merge Req

---
+++ Home_Work_1/Home_Work_1/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Home_Work_1
+{
+    [ExcludeFromCodeCoverage]
+    public class Calculator
+    {
+
+        public enum operations
+        {
+            Plus,
+            Minus,
+            Mult,
+            Divide,
+            UnknownOperation,
+        }
+        
+        public static Exception DivideZero=new DivideByZeroException("can not divided by zero");
+        public static Exception WrongOperator=new ArgumentException("this operator does not exist");
+        public static Exception OutOfRangeExcep=new ArgumentOutOfRangeException("out of range");
+        
+
+        public static int Calculate(int a,int b,operations operation)
+        {
+            var result = 0;
+            switch (operation)
+            {
+                case operations.Plus:
+                    result = a + b; 
+                    break;
+                case operations.Minus:
+                    result = a - b; 
+                    break;
+                case operations.Mult:
+                    result = a * b; 
+                    break;
+                case operations.Divide:
+                    if (b == 0)
+                    {
+                        throw DivideZero;
+                    }
+                    result = a / b; 
+                    break;
+                case operations.UnknownOperation:
+                    throw WrongOperator;
+                default:
+                    throw OutOfRangeExcep;
+            }
+            return result;
+        }
+
+        
+    }
+}
---
+++ Home_Work_1/Home_Work_1_Tests/UnitTest1.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Home_Work_1;
+
+namespace Home_Work_1_Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Minus()
+        {
+            Assert.AreEqual(32 - 21, Calculator.Calculate(32, 21, Calculator.operations.Minus));
+            Assert.AreEqual(140 - 232, Calculator.Calculate(140, 232, Calculator.operations.Minus));
+            Assert.AreEqual(656 - 212, Calculator.Calculate(656, 212, Calculator.operations.Minus));
+            Assert.AreEqual(10002 - 3232, Calculator.Calculate(10002, 3232, Calculator.operations.Minus));
+        }
+
+        [TestMethod]
+        public void Mult()
+        {
+            Assert.AreEqual(32 * 21, Calculator.Calculate(32, 21, Calculator.operations.Mult));
+            Assert.AreEqual(140 * 232, Calculator.Calculate(140, 232, Calculator.operations.Mult));
+            Assert.AreEqual(656 * 212, Calculator.Calculate(656, 212, Calculator.operations.Mult));
+            Assert.AreEqual(1002 * 322, Calculator.Calculate(1002, 322, Calculator.operations.Mult));
+        }
+
+        [ExcludeFromCodeCoverage]
+        [TestMethod]
+        public void Divide()
+        {
+            try
+            {
+                Calculator.Calculate(6, 0, Calculator.operations.Divide);
+            }
+            
+            catch (Exception e)
+            {
+                Assert.AreEqual(e,Calculator.DivideZero);
+            }
+            
+            Assert.AreEqual(5/2,Calculator.Calculate(5,2,Calculator.operations.Divide));
+            Assert.AreEqual(9/3,Calculator.Calculate(9,3,Calculator.operations.Divide));
+            Assert.AreEqual(2/3,Calculator.Calculate(2,3,Calculator.operations.Divide));
+        }
+
+        [TestMethod]
+        public void Plus()
+        {
+            Assert.AreEqual(3232+2,Calculator.Calculate(3232,2,Calculator.operations.Plus));
+            Assert.AreEqual(32+2,Calculator.Calculate(32,2,Calculator.operations.Plus));
+            Assert.AreEqual(555+313,Calculator.Calculate(555,313,Calculator.operations.Plus));
+            Assert.AreEqual(4+567,Calculator.Calculate(4,567,Calculator.operations.Plus));
+            Assert.AreEqual(11111+5631,Calculator.Calculate(11111,5631,Calculator.operations.Plus));
+        }
+
+        [ExcludeFromCodeCoverage]
+        [TestMethod]
+        public void IsOperatorRight()
+        {
+            try
+            {
+                Calculator.Calculate(3232, 3232, (Calculator.operations) 10);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(Calculator.OutOfRangeExcep,e);
+            }
+            
+            
+            try
+            {
+                Calculator.Calculate(3232, 3232, default);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(Calculator.WrongOperator, e);
+            }
+            
+        }
+
+        [TestMethod]
+        public void CoreProcess()
+        {
+            
+            Assert.AreEqual(0, Home_Work_1.Program.Main(new [] {""}));
+            Assert.AreEqual(1, Program.Main(new [] {"r","+","t"}));
+            Assert.AreEqual(2, Program.Main(new [] {"1","t","1"}));
+            Assert.AreEqual(3, Program.Main(new [] {"31","+","31"}));
+        }
+        
+        [TestMethod]
+        public void Parser()
+        {
+            Assert.AreEqual(false, Home_Work_1.Parser.IsInt("zxc", out _));
+            
+            Assert.AreEqual(true, Home_Work_1.Parser.IsInt("32", out var res));
+            Assert.AreEqual(32, res);
+            
+            Assert.AreEqual(Calculator.operations.Mult, Home_Work_1.Parser.OperationDetector("*"));
+            Assert.AreEqual(Calculator.operations.Divide, Home_Work_1.Parser.OperationDetector("/"));
+            Assert.AreEqual(Calculator.operations.Minus,Home_Work_1.Parser.OperationDetector("-"));
+            
+        }
+
+    }
+}
