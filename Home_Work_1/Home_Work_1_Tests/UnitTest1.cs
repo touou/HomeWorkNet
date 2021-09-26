ba1cf0906e990db5823f6e1e2064b8a30dc98725
touou <72223183+touou@users.noreply.github.com>
2021-09-26 17:32:51 +0300

    IL DONE

--- Home_Work_1/Home_Work_1_Tests/UnitTest1.cs
+++ Home_Work_1/Home_Work_1_Tests/UnitTest1.cs
@@ -223,6 +223,193 @@ namespace Home_Work_1_Tests
             //assert
             Assert.AreEqual(Calculator.operations.Mult, resultMult);
         }
+        
+        
+        //IL Tests
+        //IL Tests
+        //IL Tests
+        
+        [TestMethod]
+        public void MinusIL()
+        {
+            //arrange
+            var a = 10;
+            var b = 2;
+            var operation = ILzxc.Calculator.operations.Minus;
+            //act
+            var result = ILzxc.Calculator.Calculate(10, 2, operation);
+            //assert
+            Assert.AreEqual(8, result);
+        }
 
+        [TestMethod]
+        public void MultIL()
+        {
+            //arrange
+            var a = 10;
+            var b = 11;
+            var operation = ILzxc.Calculator.operations.Mult;
+            //act
+            var result = ILzxc.Calculator.Calculate(10, 11, operation);
+            //assert
+            Assert.AreEqual(110, result);
+        }
+
+        [ExcludeFromCodeCoverage]
+        [TestMethod]
+        public void DivideIL()
+        {
+            //arrange
+            var a = 10;
+            var b = 5;
+            var operation = ILzxc.Calculator.operations.Divide;
+            //act
+            var result = ILzxc.Calculator.Calculate(10, 5, operation);
+            //assert
+            Assert.AreEqual(2, result);
+            
+            
+        }
+
+        [TestMethod]
+        public void PlusIL()
+        {
+            //Arrange
+            var a = 10;
+            var b = 5;
+            var operation = ILzxc.Calculator.operations.Plus;
+            
+            //Act
+            var result = ILzxc.Calculator.Calculate(10, 5, operation);
+            
+            //Assert
+            Assert.AreEqual(15, result);
+        }
+
+        
+        [TestMethod] 
+        public void IsElementsEnoughIL()
+        {
+            //arrange
+            var point0 = 0;
+            
+            //act
+            var result0 = Home_Work_1.Program.Main(new[] {""});
+
+            //assert
+            Assert.AreEqual(point0, result0);
+        }
+
+        [TestMethod] 
+        public void IsElementsIntegerIL()
+        {
+            //arrange
+            var point1 = 1;
+            //act
+            var result1 = Program.Main(new[] {"z", "+", "q"});
+            //assert
+            Assert.AreEqual(point1, result1);
+        }
+
+        [TestMethod] 
+        public void IsOperatorAvailableIL()
+        {
+            //arrange
+            var point2 = 2;
+            //act
+            var result2 = Program.Main(new[] {"1", "zx", "1"});
+            //assert
+            Assert.AreEqual(point2, result2);
+        }
+
+        [TestMethod]
+        public void IsDoneIL()
+        {
+            //arrange
+            var point3 = 3;
+            //act
+            var result3 = Program.Main(new[] {"31", "+", "31"});
+            //assert
+            Assert.AreEqual(point3, result3);
+        }
+        
+        
+
+        [TestMethod]
+        public void ElementIsStringIL()
+        {
+            //arrange
+            var str1 = "zxc";
+            //act
+            var result1 = ILzxc.Parser.IsInt(str1, out _);
+            //assert
+            Assert.IsFalse(result1);
+        }
+        
+        [TestMethod]
+        public void ElementIsIntegerIL()
+        {
+            //arrange
+            var str2 = "32";
+            //act
+            var result2 = ILzxc.Parser.IsInt(str2, out var res1);
+            //assert
+            Assert.IsTrue(result2);
+        }
+        
+        [TestMethod]
+        public void IsElementConvertsRightIL()
+        {
+            //arrange
+            var str3 = "31";
+            //act
+            var result3 = ILzxc.Parser.IsInt(str3, out var res2);
+            //assert
+            Assert.AreEqual(31,res2);
+        }
+        
+        [TestMethod]
+        public void IsOperationRight_PlusIL()
+        {
+            //arrange
+            var symbolPlus = "+";
+            //act
+            var resultPlus = ILzxc.Parser.OperationDetector(symbolPlus);
+            //assert
+            Assert.AreEqual(ILzxc.Calculator.operations.Plus, resultPlus);
+        }
+
+        [TestMethod]
+        public void IsOperationRight_MinusIL()
+        {
+            //arrange
+            var symbolMinus = "-";
+            //act
+            var resultMinus = ILzxc.Parser.OperationDetector(symbolMinus);
+            //assert
+            Assert.AreEqual(ILzxc.Calculator.operations.Minus, resultMinus);
+        }
+
+        [TestMethod]
+        public void IsOperationRight_DivideIL()
+        {
+            //arrange
+            var symbolDivide = "/";
+            //act
+            var resultDivide = ILzxc.Parser.OperationDetector(symbolDivide);
+            //assert
+            Assert.AreEqual(ILzxc.Calculator.operations.Divide, resultDivide);
+        }
+
+        [TestMethod]
+        public void IsOperationRight_MultIL()
+        {
+            //arrange
+            var symbolMult = "*";
+            //act
+            var resultMult = ILzxc.Parser.OperationDetector(symbolMult);
+            //assert
+            Assert.AreEqual(ILzxc.Calculator.operations.Mult, resultMult);
+        }
     }
 }
