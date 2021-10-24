9c8b2e452dc54374e9085198165edaf764e1bbb2
touou <72223183+touou@users.noreply.github.com>
2021-10-24 15:36:43 +0300

    HW5

---
+++ FSLibraryHW5/CalculatorHW5.fs
@@ -0,0 +1,44 @@
+﻿namespace FSLibraryHW5
+
+type ResultBuilder(errorMessage: string) =
+    new() = ResultBuilder("error")
+    member v1.Zero() = Error errorMessage
+
+    member v1.Bind(x, f) =
+        match x with
+        | Ok x -> f x
+        | Error _ -> x
+
+    member v1.Return x = Ok x
+    member v1.Combine(x, f) = f x
+
+module CalculatorHW5 =
+    
+    type Operation =
+        | Plus
+        | Minus
+        | Divide
+        | Mult
+        
+    let private defaultResult = ResultBuilder("unknown error")
+    let devByZero = "you can't dived by zero"
+
+    let inline calculate
+        (a1: Result<'T, string> when 'T: (static member (+) : 'T * 'T -> 'T) and 'T: (static member (/) :
+                   'T * 'T -> 'T) and 'T: (static member (*) : 'T * 'T -> 'T) and 'A: (static member (-) : 'T * 'T -> 'T))
+        (a2: Result<'T, string>)
+        (operation: Result<Operation, string>)=
+        match operation with
+        | Ok operation ->
+            ResultBuilder(devByZero) {
+                let! val1 = a1
+                let! val2 = a2
+                match operation with
+                | Plus -> return val1 + val2
+                | Divide ->
+                    if val2 <> new 'T() then
+                        return val1 / val2
+                | Minus -> return val1 - val2
+                | Mult -> return val1 * val2
+            }
+        | Error operationError -> Error operationError
---
+++ FSLibraryHW5/ParserHW5.fs
@@ -0,0 +1,47 @@
+﻿namespace FSLibraryHW5
+
+open System
+open FSLibraryHW5
+open FSLibraryHW5.CalculatorHW5
+
+module ParserHW5 = 
+    let eMessage = "the value wasn't int"
+    let notOperation = "this operation doesn't exist"
+    let private resultNumber = ResultBuilder(eMessage)
+    
+    
+    let getOperator operator =
+        ResultBuilder(notOperation) {
+            if (operator="+" || operator="/" || operator="-" || operator="*") then
+                match operator with
+                | "+" -> return Operation.Plus
+                | "-" -> return Operation.Minus
+                | "*" -> return Operation.Mult
+                | _ -> return Operation.Divide
+        }
+
+    let intP (str: string) =
+        resultNumber {
+            let success, result = Int32.TryParse str
+            if success then return result
+        }
+        
+    let decimalP (str: string) =
+        resultNumber {
+            let success, result = Decimal.TryParse str
+            if success then return result
+        }
+
+    let doubleP (str: string) =
+        resultNumber {
+            let success, result = Double.TryParse str
+            if success then return result
+        }
+
+    let floatP (str: string) =
+        resultNumber {
+            let success, result = Single.TryParse str
+            if success then return result
+        }
+
+    
